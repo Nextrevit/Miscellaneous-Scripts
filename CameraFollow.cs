01ac80c2b62f3059793d2a445ce7983eae839828
Nextrevit <38021550+NextrevitYT@users.noreply.github.com>
2020-06-25 23:15:58 -0400

    Create CameraFollow.cs

---
+++ CameraFollow.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject objectToFollow;
+
+    public float speed = 2.0f;
+
+    void Update()
+    {
+        float interpolation = speed * Time.deltaTime;
+
+        Vector3 position = this.transform.position;
+        position.y = Mathf.Lerp(this.transform.position.y, objectToFollow.transform.position.y, interpolation);
+        position.x = Mathf.Lerp(this.transform.position.x, objectToFollow.transform.position.x, interpolation);
+
+        this.transform.position = position;
+    }
+}
+
+// From unity, used in-game
